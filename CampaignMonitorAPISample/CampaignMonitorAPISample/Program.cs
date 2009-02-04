5eda7303ab9e419d65e8bbc3820d442d60866cba
Travis Bell <travisbell@me.com>
2009-02-04 12:07:48 +1100

    Fixed some typos.

--- CampaignMonitorAPISample/CampaignMonitorAPISample/Program.cs
+++ CampaignMonitorAPISample/CampaignMonitorAPISample/Program.cs
@@ -8,10 +8,6 @@ namespace CampaignMonitorAPISample
 {
     class Program
     {
-        string apiKey = "1679b865f887884e4c3917bf73e92c2f";
-        string listID = "aabd28c266ba32e4d7d91b4a4c47deb1";
-        string emailAddress = "jasonh+1@freshview.com";
-
         static void Main(string[] args)
         {
             #region Subscriber. operations
